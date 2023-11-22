b33763d2615ef770692eb4311ae379223abd4f1e
Laura Tavares <144948480+LauraTavares2323@users.noreply.github.com>
2023-11-22 15:17:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+// // // Coleta e Análise de Dados do Usuário
+// let nome 
+// let cidade 
+
+// console.log(typeof nome, typeof cidade)
+
+// nome = prompt("Digite o seu nome")
+// cidade = prompt("Digite sua cidade")
+
+// console.log(typeof nome, typeof cidade)
+
+// console.log("Olá, meu nome é", nome, "e eu moro em", cidade)
+
+// // // Perguntas Sim ou Não
+
+// let pergunta1
+// let pergunta2
+// let pergunta3
+
+// pergunta1 = String(prompt("Você gosta de programar? [sim] [não]"))
+// pergunta2 = String(prompt("Você já viajou para fora do país? ]sim] [não]"))
+// pergunta3 = String(prompt("Você tem animais de estimação? [sim] [não]"))
+
+// console.log(pergunta1)
+// console.log(pergunta2)
+// console.log(pergunta3)
+
+// // Troca de Valores entre Variáveis 
+
+// let X = 2
+// let Y = 4
+
+// let novoy = X
+// let novox = Y
+
+// X === novox
+// Y === novoy
+
+// console.log(X, Y)
+
+// Operações Matemáticas com Entrada do Usuário 
+
+let valor1
+let valor2
+
+valor1 = Number(prompt("Insira seu primeiro valor"))
+valor2 = Number(prompt("Insira seu segundo valor"))
+
+let X = valor1 + valor2
+let Y = valor1 * valor2
+
+console.log("A soma dos números é:", X)
+console.log(" A multiplicação dos números é:", Y)
